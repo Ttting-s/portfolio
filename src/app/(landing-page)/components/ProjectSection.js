1a42b44226170e3bb9c234e7e4bdb5d86cebe0e0
Ting Shen <z5554386@student.unsw.edu.au>
2025-12-20 00:10:43 +1100

    responsiveness

--- src/app/(landing-page)/components/ProjectSection.js
+++ src/app/(landing-page)/components/ProjectSection.js
@@ -66,7 +66,7 @@ const ProjectSection = () =>{
       <h2 className="text-3xl font-bold text-white mb-12 text-center">
         Featured Projects
       </h2>
-      <div className="grid grid-cols-1 gap-4 sm:grid-cols-3">
+      <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-3">
         {projects.map((project, index) => (
           <button
             onClick={() => handleProjectClick(project.id)}
